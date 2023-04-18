a8169474e68d23408671568feda30b9fc3d1916c
koskv <koskv@list.ru>
2023-04-18 18:04:20 +0300

    v 1.0.7 - driver changed to firefox

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -8,6 +8,7 @@ import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.remote.*;
 
 
 public class BaseTest {
@@ -15,13 +16,25 @@ public class BaseTest {
     public final static String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
 
     public void setUp() {
-        WebDriverManager.firefoxdriver().setup(); // Установили настройки драйвера
+
+        WebDriverManager.firefoxdriver().driverVersion("0.32.1").setup(); // Установили настройки драйвера
         Configuration.browser = "firefox";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
         Configuration.driverManagerEnabled = true;
         Configuration.headless = true;
         Configuration.fastSetValue=true;
+
+     /*   Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl="https://qa-scooter.praktikum-services.ru/";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+*/
+
     }
 
     @Before
